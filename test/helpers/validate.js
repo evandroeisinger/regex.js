7229f66b76351544b99e17100c9c1e844f8e8666
Evandro Eisinger <evandroeisinger@gmail.com>
2015-08-28 19:53:42 -0300

    added validate helper tests

--- test/helpers/validate.js
+++ test/helpers/validate.js
@@ -1,7 +1,14 @@
 'use strict';
 
+var assert = require('assert');
 var validateHelper = require('../../helpers/validate');
 
 describe('helpers/validate', function () {
-  // TODO
+  it('should a validate a regex', function () {
+    var regex = /^test$/;
+    regex.validate = validateHelper;
+
+    assert.equal(regex.validate('test'), true);
+    assert.equal(regex.validate('tset'), false);
+  });
 });
